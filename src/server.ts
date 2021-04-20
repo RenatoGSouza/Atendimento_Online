0612d17e365a8da3916c13543e55fbb2a29304ab
renatogsouza <renatomacedo1995@gmail.com>
2021-04-20 11:37:26 -0300

    configurando o typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express, { response } from "express";
+import express, { response } from 'express';
+
+import './database';
 
 const app = express();
 /*
@@ -9,12 +11,13 @@ PATCH = Alterar uma informação específica
 */
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
+
 app.get('/', (request, response) => {
   return response.json({
     message: "Hello World",
   });
 });
 
-// app.post('/', (request, response) => {
-//   return response.json({message: 'Usuário salvo com sucesso!'});
-// });
+app.post('/', (request, response) => {
+  return response.json({message: 'Usuário salvo com sucesso!'});
+});
