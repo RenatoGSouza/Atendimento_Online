c5776a58674e8e90dc7082d76054844796ad03c6
renatogsouza <renatomacedo1995@gmail.com>
2021-04-20 11:08:13 -0300

    inicio do projeto de assistente virtual

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { response } from "express";
+
+const app = express();
+/*
+GET = Buscas
+POST = Criação
+DELETE = Deletar
+PATCH = Alterar uma informação específica
+*/
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
+app.get('/', (request, response) => {
+  return response.json({
+    message: "Hello World",
+  });
+});
+
+// app.post('/', (request, response) => {
+//   return response.json({message: 'Usuário salvo com sucesso!'});
+// });
