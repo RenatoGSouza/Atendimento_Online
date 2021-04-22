12571a2423b4bbf2b7dfae7d89a699c1327ad161
renatogsouza <renatomacedo1995@gmail.com>
2021-04-22 10:49:06 -0300

    adiciona arquivos front-end

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
 
 import './database';
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (request, response) =>{
+  return;
+});
+
+const http = createServer(app);//Criando protocolo http
+const io = new Server(http);//Criando protocolo ws
+
+io.on('connection', (socket: Socket) => {
+  console.log('Se conectou', socket.id);
+});
+
 app.use(express.json())
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+http.listen(3333, () => console.log('Server is running on port 3333'));
